e5601445f2ae54d7b406ccb38149fb0ff42a66c6
Mihirpndya <mpandya284@gmail.com>
2024-05-05 17:21:32 +0530

    backend completed

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+//mongodb+srv://mihir_pandya:XVEOLFJe623HVE0u@cluster0.jyjb85p.mongodb.net/
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://mihir_pandya:Ney7007@cluster0.jyjb85p.mongodb.net/todo');
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean,
+});
+
+const todoModel = mongoose.model('todo',todoSchema);
+
+module.exports = {
+    todoModel,
+}
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,64 @@
 const express = require("express");
+const { todoModel } = require("./db");
 const port = 3000;
 const app = express();
+const cors = require("cors");
+const { todoSchema, updateSchema } = require("./types");
 
-app.get("/getTodos", (req, res) => {
-	res.send("hi");
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/addTodos", async (req, res) => {
+	const inputPayload = req.body;
+	const parsedPayload = todoSchema.safeParse(inputPayload);
+
+	if (!parsedPayload.success) {
+		res.status(411).json({
+			msg: "You have provided invalid inputs please cross-check",
+		});
+    return;
+	}
+
+	await todoModel.create({
+		title: inputPayload.title,
+		description: inputPayload.description,
+	});
+
+  res.json({
+    msg:"Todo Created"
+  })
+});
+
+
+app.get("/getTodos", async (req, res) => {
+
+  const outputPayload = await todoModel.find({});
+
+  res.json({
+    outputPayload
+  })
+
+});
+
+app.put("/completed",async (req, res) => {
+	const updatePayload = req.body;
+  const parsedPayload = updateSchema.safeParse(updatePayload);
+  if(!parsedPayload.success){
+    res.status(411).json({
+      msg:"Invalid Inputs"
+    })
+    return;
+  }
+  await todoModel.update({
+    _id: updatePayload.id,
+  },{
+    completed: true,
+  })
+
+  res.json({
+    msg:"Todo marked as done"
+  })
 });
 
 app.listen(port, () => {
